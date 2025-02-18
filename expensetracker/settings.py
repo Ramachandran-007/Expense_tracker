7d17bb44e3cf473543a08cdd704f1e7c52a11264
Ramachandran M D <ramachandran1840854@gmail.com>
2025-02-18 13:09:45 +0530

    setting.py

--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -158,9 +158,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'hemantshirsath24@gmail.com'
-EMAIL_HOST_PASSWORD = 'gjxu zgem wvtv vklu'
-DEFAULT_FROM_EMAIL = 'hemantshirsath24@gmail.com'
+EMAIL_HOST_USER = 'unavailablerror404@gmail.com'
+EMAIL_HOST_PASSWORD = 'kudp hpvw zlfl klly'
+DEFAULT_FROM_EMAIL = 'unavailablerror404@gmail.com'
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
